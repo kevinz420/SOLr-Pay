a8ae185e5d0ed11cfe056ad040555be09abc728a
Varun <varunsiva360@gmail.com>
2022-07-07 00:58:36 -0500

    combined instructions

--- app/src/utils/edit-profile.ts
+++ app/src/utils/edit-profile.ts
@@ -1,8 +1,9 @@
 import getProvider from "./get-provider";
 import getProgram from "./get-program";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey, Transaction } from "@solana/web3.js";
 import * as anchor from "@project-serum/anchor";
 import { WalletContextState } from "@solana/wallet-adapter-react";
+const sendAndConfirmTransaction = require("solana/web3.js");
 
 export default async function editProfile(
   wallet: WalletContextState,
@@ -11,6 +12,7 @@ export default async function editProfile(
   oldname?: string,
   pfp?: Buffer
 ) {
+  let empty: boolean = true;
   const provider = await getProvider(wallet, connection);
   const program = await getProgram(wallet, connection);
 
@@ -21,6 +23,8 @@ export default async function editProfile(
     ],
     program.programId
   );
+
+  const tx = new Transaction();
 
   if (username && oldname) {
     const [nickPDA] = await PublicKey.findProgramAddress(
@@ -39,26 +43,50 @@ export default async function editProfile(
       program.programId
     );
 
-    await program.methods
-      .changeUsername(username)
-      .accounts({
-        nickname: nickPDA,
-        profile: profilePDA,
-        newNickname: updatedNickPDA,
-        user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        sysvarRent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      })
-      .rpc();
+    if (username !== oldname) {
+      tx.add(
+        await program.methods
+          .changeUsername(username)
+          .accounts({
+            nickname: nickPDA,
+            profile: profilePDA,
+            newNickname: updatedNickPDA,
+            user: provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+            sysvarRent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          })
+          .instruction()
+        );
+      empty = false;
+    }
   }
 
   if (pfp) {
-    await program.methods
+    tx.add(
+      await program.methods
       .changePfp(pfp)
       .accounts({
         profile: profilePDA,
         user: provider.wallet.publicKey,
       })
-      .rpc();
+      .instruction()
+    );
+    empty = false;
+  }
+
+  if (empty) {
+    //maybe return an error on frontend since they entered nothing
+  } else {
+    const txid = await sendAndConfirmTransaction(connection, tx, [provider.wallet.publicKey], {    //not sure if this will work, this uses *solana web3.js
+      skipPreflight: true,
+      preflightCommitment: "confirmed",
+      confirmation: "confirmed",
+    });
+
+    //^ if this doesn't work, replace it with:
+    // const txid = await sendAndConfirmTransaction(connection, tx, [provider.wallet.publicKey]);
+  
+    //optional log if needed
+    //console.log(`http://explorer.solana.com/tx/${txid}?cluster=devnet`)
   }
 }
