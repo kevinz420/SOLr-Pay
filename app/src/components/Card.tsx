21eaf3b7eb6649d5fcea7cf3b02bb1a56f612971
Varun <varunsiva360@gmail.com>
2022-08-08 15:23:51 -0500

    card

--- app/src/components/Card.tsx
+++ app/src/components/Card.tsx
@@ -14,19 +14,21 @@ export const Card: React.FC<CardProps> = (props) => {
   return (
     <>
       <div
-        className="bg-gray-900 p-2 mb-2 text-white rounded-xl flex flex-col gap-3 relative h-28 pl-12 pr-5 w-3/4"
+        className="bg-gray-800 p-0 text-white rounded-none relative gap-0 self-end flex flex-col h-28 w-11/12 mb-0 md:pl-10 md:pr-5 md:bg-gray-900 md:mb-2 md:p-2 md:rounded-xl md:gap-3 md:w-3/4"
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <div className="absolute w-5 h-3/4 left-0 bg-gray-700 rounded-r-lg top-3"></div>
+        <div className="hidden absolute w-5 h-3/4 left-0 bg-gray-700 rounded-r-lg top-3 md:block"></div>
         <div
           className={classNames(
-            "absolute w-2 h-1/2 left-1.5  top-6 rounded-lg",
+            "hidden md:block absolute w-2 h-1/2 left-1.5 top-6 rounded-lg",
             hover ? "bg-gradient-to-t from-neutral-400 to-white" : "bg-gray-500"
           )}
         ></div>
         {props.children}
       </div>
+
+      <div className="block h-px self-end w-11/12 bg-gray-500 md:hidden"></div>
     </>
   );
 };
